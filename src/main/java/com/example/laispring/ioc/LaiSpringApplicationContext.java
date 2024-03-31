54b82260ef47c3f68b22dbc6ca5a10d770b6fade
zurichscud <zurichscud@outlook.com>
2024-03-31 20:14:22 +0800

    fix 未检查的类型等问题

--- src/main/java/com/example/laispring/ioc/LaiSpringApplicationContext.java
+++ src/main/java/com/example/laispring/ioc/LaiSpringApplicationContext.java
@@ -24,10 +24,10 @@ public class LaiSpringApplicationContext {
     public static final String PROTOTYPE = "prototype";
     private final Class<LaiSpringConfig> configClass;
     private final ConcurrentHashMap<String, BeanDefinition> beanDefinitionMap = new ConcurrentHashMap<>();
-    private final ConcurrentHashMap<String, Object> singtonObjects = new ConcurrentHashMap<>();
+    private final ConcurrentHashMap<String, Object> singletonObjects = new ConcurrentHashMap<>();
     private URL resource;
     private String componentScanValue;
-    private ArrayList<BeanPostProcessor> processors=new ArrayList<>();
+    private final ArrayList<BeanPostProcessor> processors=new ArrayList<>();
 
     public LaiSpringApplicationContext(Class<LaiSpringConfig> configClass) {
         this.configClass = configClass;
@@ -74,46 +74,49 @@ public class LaiSpringApplicationContext {
     private void getBeanDefinitionMap() {
         File file = new File(resource.getPath());
         File[] files = file.listFiles();
-        for (File temp : files) {
-            //System.out.println(temp);
-            String absolutePath = temp.getAbsolutePath();
-            String className = absolutePath.substring(absolutePath.lastIndexOf("\\") + 1, absolutePath.indexOf(".class"));
-            //System.out.println(className);//获取类名
-            String fullName = componentScanValue.replace("/", ".") + "." + className;
-            //System.out.println(fullName);
-            try {
-                Class<?> clazz = Class.forName(fullName);
-                if (clazz.isAnnotationPresent(Controller.class) ||
-                        clazz.isAnnotationPresent(Component.class) ||
-                        clazz.isAnnotationPresent(Service.class) ||
-                        clazz.isAnnotationPresent(Repository.class)) {
-                    if (BeanPostProcessor.class.isAssignableFrom(clazz)) {
-                        //如果是处理器则直接创建，跳出循环
-                        createProcessor(clazz);
-                        continue;
-                    }
-                    if (clazz.isAnnotationPresent(Scope.class)) {
+        if (files != null) {
+            for (File temp : files) {
+                //System.out.println(temp);
+                String absolutePath = temp.getAbsolutePath();
+                String className = absolutePath.substring(absolutePath.lastIndexOf("\\") + 1, absolutePath.indexOf(".class"));
+                //System.out.println(className);//获取类名
+                String fullName = componentScanValue.replace("/", ".") + "." + className;
+                //System.out.println(fullName);
+                try {
+                    Class<?> clazz = Class.forName(fullName);
+                    if (clazz.isAnnotationPresent(Controller.class) ||
+                            clazz.isAnnotationPresent(Component.class) ||
+                            clazz.isAnnotationPresent(Service.class) ||
+                            clazz.isAnnotationPresent(Repository.class)) {
+                        if (BeanPostProcessor.class.isAssignableFrom(clazz)) {
+                            //如果是处理器则直接创建，跳出循环
+                            createProcessor(clazz);
+                            continue;
+                        }
+                        if (clazz.isAnnotationPresent(Scope.class)) {
 
-                        String scope = clazz.getDeclaredAnnotation(Scope.class).value();
-                        if (PROTOTYPE.equals(scope)) {
+                            String scope = clazz.getDeclaredAnnotation(Scope.class).value();
+                            if (PROTOTYPE.equals(scope)) {
+                                BeanDefinition beanDefinition = new BeanDefinition();
+                                beanDefinition.setScope(scope);
+                                beanDefinition.setClazz(clazz);
+                                beanDefinitionMap.put(StringUtils.uncapitalize(className), beanDefinition);
+                            } else throw new RuntimeException("类型错误");
+                        } else {
                             BeanDefinition beanDefinition = new BeanDefinition();
-                            beanDefinition.setScope(scope);
+                            beanDefinition.setScope("singleton");
                             beanDefinition.setClazz(clazz);
                             beanDefinitionMap.put(StringUtils.uncapitalize(className), beanDefinition);
-                        } else new RuntimeException("类型错误");
-                    } else {
-                        BeanDefinition beanDefinition = new BeanDefinition();
-                        beanDefinition.setScope("singleton");
-                        beanDefinition.setClazz(clazz);
-                        beanDefinitionMap.put(StringUtils.uncapitalize(className), beanDefinition);
+                        }
+
                     }
-
+                } catch (Exception e) {
+                    throw new RuntimeException(e);
                 }
-            } catch (Exception e) {
-                throw new RuntimeException(e);
-            }
 
+            }
         }
+        throw new RuntimeException("文件夹为空");
     }
 
     /**
@@ -126,16 +129,14 @@ public class LaiSpringApplicationContext {
             Object object = clazz.newInstance();
             BeanPostProcessor beanPostProcessor = (BeanPostProcessor) object;
             processors.add(beanPostProcessor);
-        } catch (InstantiationException e) {
-            throw new RuntimeException(e);
-        } catch (IllegalAccessException e) {
+        } catch (Exception e) {
             throw new RuntimeException(e);
         }
 
     }
 
     private void getComponentScanURL() {
-        ComponentScan componentScan = (ComponentScan) this.configClass.getDeclaredAnnotation(ComponentScan.class);
+        ComponentScan componentScan = this.configClass.getDeclaredAnnotation(ComponentScan.class);
         componentScanValue = componentScan.value();
         //System.out.println(componentScanValue);
         String replace = componentScanValue.replace(".", "/");
@@ -146,7 +147,7 @@ public class LaiSpringApplicationContext {
     private void initSingletonObjects() {
         for (Map.Entry<String, BeanDefinition> entry : beanDefinitionMap.entrySet()) {
             if (SINGLETON.equals(entry.getValue().getScope())) {
-                singtonObjects.put(entry.getKey(), createBean(entry.getKey()));
+                singletonObjects.put(entry.getKey(), createBean(entry.getKey()));
             }
         }
 
@@ -171,7 +172,7 @@ public class LaiSpringApplicationContext {
 
     private void display() {
         System.out.println("------------IOC list-------------");
-        for (Map.Entry<String, Object> entry : singtonObjects.entrySet()) {
+        for (Map.Entry<String, Object> entry : singletonObjects.entrySet()) {
             System.out.println(entry.getKey() + "=" + entry.getValue());
 
         }
@@ -185,11 +186,16 @@ public class LaiSpringApplicationContext {
     public <T> T getBean(String beanName, Class<T> clazz) {
         for (Map.Entry<String, BeanDefinition> entry : beanDefinitionMap.entrySet()) {
             if (beanName.equals(entry.getKey())) {
+                Object object;
                 if (SINGLETON.equals(entry.getValue().getScope())) {
-                    return ((T) singtonObjects.get(beanName));
+                    object = singletonObjects.get(beanName);
+
                 } else {
-                    Object object = createBean(entry.getKey());
-                    return ((T) object);
+                    object = createBean(entry.getKey());
+
+                }
+                if (clazz.isInstance(object)){
+                    return clazz.cast(object);
                 }
 
             }
@@ -198,13 +204,13 @@ public class LaiSpringApplicationContext {
     }
 
     /**
-     * @Description: 根据beanNaeme创建bean
+     * @Description: 根据beanName创建bean
      * @Param:
      * @Return:
      **/
     private Object createBean(String beanName) {
         BeanDefinition beanDefinition = beanDefinitionMap.get(beanName);
-        Object object = null;
+        Object object ;
         try {
             object = beanDefinition.getClazz().newInstance();
             autowired(object);
@@ -220,9 +226,7 @@ public class LaiSpringApplicationContext {
             }
             /*after init*/
 
-        } catch (InstantiationException e) {
-            throw new RuntimeException(e);
-        } catch (IllegalAccessException e) {
+        } catch (Exception e) {
             throw new RuntimeException(e);
         }
         return object;
